eef6189b7575ee179d57605cf1ae3e71feb1457c
gyubin <gyubinhhj@naver.com>
2025-01-09 21:11:52 +0900

    프런트 단 자바스크립트 파일 만들기

--- app/app.js
+++ app/app.js
@@ -11,6 +11,8 @@ const home = require("./src/routes/home/index.js");
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static('${__dirname}src/public'));
+
 
 app.use("/", home); //use -> 미드웨어를 들롱해주는 메서드
 
