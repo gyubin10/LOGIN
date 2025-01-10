cf4460255c430161dddbd1e41e98d524ffb7341b
gyubin <gyubinhhj@naver.com>
2025-01-10 21:24:50 +0900

    nodemon 적용

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 //모듈
 const express = require("express");
 const app = express();
+const path = require('path');
 
 
 //라우팅
@@ -11,7 +12,7 @@ const home = require("./src/routes/home/index.js");
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-app.use(express.static('${__dirname}src/public'));
+app.use(express.static(path.join(__dirname, 'src', 'public')));
 
 
 app.use("/", home); //use -> 미드웨어를 들롱해주는 메서드
